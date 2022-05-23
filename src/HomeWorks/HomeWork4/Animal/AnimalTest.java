e666a31acf207977da125705a70905a7920aca0a
YK <enskynet@gmail.com>
2022-05-23 21:46:55 +0300

    HW4 Animal add.

---
+++ src/HomeWorks/HomeWork4/Animal/AnimalTest.java
@@ -0,0 +1,18 @@
+package HomeWorks.HomeWork4.Animal;
+
+import java.util.Scanner;
+
+public class AnimalTest {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.animalSet();
+        animal.animalGet();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter name, speed, age: ");
+        String name = scanner.next();
+        int speed = scanner.nextInt();
+        int age = scanner.nextInt();
+        animal.animalSet(name, speed, age);
+        animal.animalGet();
+    }
+}
