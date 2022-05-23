e0c96a371b94614eb495e58419c4038e9a8c0f29
YK <enskynet@gmail.com>
2022-05-23 23:22:08 +0300

    HW5 Warrior in progress.

---
+++ src/HomeWorks/HomeWork5/Warrior/Sword.java
@@ -0,0 +1,32 @@
+package HomeWorks.HomeWork5.Warrior;
+
+public class Sword extends Warrior{
+    private int health;
+    private int damage;
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    @Override
+    void attack() {
+
+    }
+
+    @Override
+    void Warrior() {
+
+    }
+}
---
+++ src/HomeWorks/HomeWork5/Warrior/Warrior.java
@@ -0,0 +1,6 @@
+package HomeWorks.HomeWork5.Warrior;
+
+abstract class Warrior {
+    abstract void attack();
+    abstract void Warrior();
+}
