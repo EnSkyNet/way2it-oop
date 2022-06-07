0ce0ded6fbcd53fa17b488f3273e601efe2945b0
YK <enskynet@gmail.com>
2022-06-07 11:34:17 +0300

    Fix accounts.

--- src/homeworks/homework18/bank/UserService.java
+++ src/homeworks/homework18/bank/UserService.java
@@ -1,6 +1,9 @@
 package homeworks.homework18.bank;
 
 public interface UserService<Account, User> {
+    DebitAccount debitAccount = null;
+    CreditAccount creditAccount = null;
+
     void printAllAccount();
 
     void printAllUser();
