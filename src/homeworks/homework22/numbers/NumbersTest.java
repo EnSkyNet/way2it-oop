b022683843ed9483c07f8a2ac616e247628b6f91
YK <enskynet@gmail.com>
2022-06-11 21:11:55 +0300

    HW 22 fix.

--- src/homeworks/homework22/numbers/NumbersTest.java
+++ src/homeworks/homework22/numbers/NumbersTest.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 
 public class NumbersTest {
     public static void main(String[] args) {
-        ICalc iCalc = (one, two, three) -> (one + two + three) / 3;
-        ICalc iCalc1 = (one, two, three) -> one + two + three;
-        ICalc iCalc2 = (one, two, three) -> {
+        ICalc averageFunction = (one, two, three) -> (one + two + three) / 3;
+        ICalc sumFunction = (one, two, three) -> one + two + three;
+        ICalc findMaxNumberFunction = (one, two, three) -> {
             int max = one;
             if (max < two) max = two;
             if (max < three) max = three;
@@ -21,13 +21,13 @@ public class NumbersTest {
                 ", Second number: " + two +
                 ", Third number: " + three);
 
-        int max = iCalc2.action(one, two, three);
+        int max = findMaxNumberFunction.action(one, two, three);
         System.out.println("Max number: " + max);
 
-        int middle = iCalc.action(one, two, three);
+        int middle = averageFunction.action(one, two, three);
         System.out.println("Average: " + middle);
 
-        int sum = iCalc1.action(one, two, three);
+        int sum = sumFunction.action(one, two, three);
         System.out.println("Sum: " + sum);
     }
 }
